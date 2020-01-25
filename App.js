5edb40074a347550c61b8a032fc2cee0a7da80eb
itsyourgalj <43788790+itsyourgalj@users.noreply.github.com>
2020-01-25 16:12:30 +0700

    Update App.js

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const HomeNavigation = createStackNavigator({
               color: '#f3f3f3'
           },
           headerStyle: {
-              backgroundColor: '#010001',
+              backgroundColor: '#e9b2bc',
           },
       }
   },
@@ -43,7 +43,7 @@ const HomeNavigation = createStackNavigator({
               color: '#f3f3f3'
           },
           headerStyle: {
-              backgroundColor: '#010001',
+              backgroundColor: '#e9b2bc',
           },
       }
   },
